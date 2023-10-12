550bcdb192059952e329b952cdb52ceaae28dd3b
kthawani <kthawani@entera.ai>
2023-10-12 17:45:10 +0530

    feedback form created

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function sumbitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={sumbitFormHandler}>
+        <div>
+          <label htmlFor="email">Enter Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Enter Feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
